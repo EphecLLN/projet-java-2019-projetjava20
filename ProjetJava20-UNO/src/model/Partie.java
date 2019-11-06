ed6ee9bc953d474cd77a205a4f48a6ac216553f8
flosalpietro <florencesalpietro@gmail.com>
2019-11-06 16:14:11 +0100

    uml

---
+++ ProjetJava20-UNO/src/model/Partie.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author Florence
+ *
+ */
+public class Partie {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
