2c591b937003d1277ae0c132380c2921700cd254
AmelieC <a22c2222@yahoo.fr>
2019-11-15 15:37:02 +0100

    Update Partie.java

--- ProjetJava20-UNO/src/model/Partie.java
+++ ProjetJava20-UNO/src/model/Partie.java
@@ -4,17 +4,129 @@
 package model;
 
 /**
- * @author Florence Salpietro & Amélie Courtin
+ * 
+ */
+
+/**
+ * @author Courtin
  *
  */
 public class Partie {
+	
+	public Joueur joueur;
+	public  Joueur joueurEnCours ;
+	public int sens;
+	public int nbJoueur;
+	public Joueur gagnant;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Partie(Joueur joueur, Joueur joueurEnCours, int sens, int nbJoueur, Joueur gagnant) {
+		this.joueur = joueur;
+		this.joueurEnCours = joueurEnCours;
+		this.sens = sens;
+		this.nbJoueur = nbJoueur;
+		this.gagnant = gagnant;
 	}
 
+
+	
+	
+	
+	 public void determinerSens() { 
+		 
+	 }
+	 
+	 public Joueur determinerJoueur () {
+		return joueur ;
+		 
+	 }
+	 
+	 public int calculerScore (Joueur joueur) {
+		return joueur;
+		 
+	 }
+
+
+
+	 
+	 //accesseurs mutateurs
+
+
+	public Joueur getJoueur() {
+		return joueur;
+	}
+
+
+
+
+
+	public void setJoueur(Joueur joueur) {
+		this.joueur = joueur;
+	}
+
+
+
+
+
+	public Joueur getJoueurEnCours() {
+		return joueurEnCours;
+	}
+
+
+
+
+
+	public void setJoueurEnCours(Joueur joueurEnCours) {
+		this.joueurEnCours = joueurEnCours;
+	}
+
+
+
+
+
+	public int getSens() {
+		return sens;
+	}
+
+
+
+
+
+	public void setSens(int sens) {
+		this.sens = sens;
+	}
+
+
+
+
+
+	public int getNbJoueur() {
+		return nbJoueur;
+	}
+
+
+
+
+
+	public void setNbJoueur(int nbJoueur) {
+		this.nbJoueur = nbJoueur;
+	}
+
+
+
+
+
+	public Joueur getGagnant() {
+		return gagnant;
+	}
+
+
+
+
+
+	public void setGagnant(Joueur gagnant) {
+		this.gagnant = gagnant;
+	}
+	
+
 }
+
